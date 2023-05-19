e0a5a76fd1962188bfda16e25312acb119b7c5c1
Mohammed Badat <moebadat16@gmail.com>
2023-05-19 11:09:19 +0200

    Initial Commit

---
+++ I am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by DA MAC M1 144 on 2023/05/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
